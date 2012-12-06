1297abaabdc80760860f8cc2b0d2e0a11768b4b3
Miguel Camacho <mcamacho@fullsail.edu>
2012-12-06 17:02:22 -0600

    Project 3 JS Update
    Finishing up Boolean values to make flow better, fleshing out array
    ideals to get the array part and working on number function thereafter.

--- camacho_miguel_assignment3.js
+++ camacho_miguel_assignment3.js
@@ -95,7 +95,8 @@ areaChoose(locale2);
 var Dest = locale.scene.Middle.area;
 console.log(War + " grinned, \" To the " + Dest + "!\"");
 console.log("****************************************************************************************");
-console.log(Htr + " held up his dragon-bone bow with both hands, repeling each strike from the " + Class[2] + ".");
+console.log(Htr + " held up his dragon-bone bow with both hands, repeling each strike from the " + Class[2] + ", each blow seeming to ward off the wildlife");
+console.log("that hid in the tall grass that surrounded the two.");
 //String Function (Strings, String Prop, Local Var, String Concatenation, Return String Value
 if (orcBhishma = "Warior"){
 	var DefAtk = "Shield Bash";
@@ -103,7 +104,7 @@ if (orcBhishma = "Warior"){
 };
 console.log(Htr + " took the brundt of the hit upon his shoulder, faltering back slightly.");
 console.log(War + " saw this as an opening for a charge attack.");
-//Boolean Function
+//Boolean Function (Bool Arguments, Prop, Comparison, True/False, Outputs, Return Boolean value)
 if ((Spar && Spat) || RealFight) {
     console.log(War + " feels that this is a friendly spar and should go easy on " + Htr + ".");
 } else {
@@ -112,4 +113,21 @@ if ((Spar && Spat) || RealFight) {
 console.log("Instead of " + Htr + " attempting to move to the side, the " + Class[0] + " let his momentum assit by falling towards his back in the loft grass.");
 console.log(War +  " had a great start with his attack that the drop suprised him but couldn't slow down in time as " + Htr + " moved on the ground,");
 console.log("tripping " + War + " with a bit of foot work, causing the " + Class[2] + " to loose footing and fall foward, his sword tossing to the side.");
+console.log(War + " rushed to his feet, found his sword in a frantic, and lunged to pick up the ebon blade, and then readied himself for " + Htr + ".");
+console.log(War + " face gave a puzzling look as he realized the " + Class[0] + " is nowhere in sight.  The " + Class[2] + " stood motionless, surveying the area.");
+//Array Function (Array Arguments, Object argument, mutator	, array prop, function(procedure hopefully), local var, for loop, nested loop, math, output, accessor, array return)			
+var Arrow = 4,
+Aim = ["shoulder", "forearm", "knee"],
+Notch = [1,2,3,4];
+Aim.push("ground");
 
+
+
+/*needed
+Number Argument
+Number Prop
+Number return Value
+While loop
+Returned Values
+procuedure (if nto used yet)
+Finish*/
