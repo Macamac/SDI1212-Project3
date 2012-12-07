eb8c168055f4cf06dcbb04109b703bcf651c71d9
Miguel Camacho <mcamacho@fullsail.edu>
2012-12-06 22:41:03 -0600

    Project 3 JS Update
    Completed the assignment and the flowchart as well, will be uploading
    to the dashboard now

--- camacho_miguel_assignment3.js
+++ camacho_miguel_assignment3.js
@@ -15,6 +15,8 @@ Spar = true,
 Spat = false,
 RealFight = false,
 UnDef = 20,
+Arrows = 16,
+maxSlash = 1,
 Class = ["Hunter", " Shaman", "Warrior", "Priest"]
 
 ;
@@ -115,19 +117,49 @@ console.log(War +  " had a great start with his attack that the drop suprised hi
 console.log("tripping " + War + " with a bit of foot work, causing the " + Class[2] + " to loose footing and fall foward, his sword tossing to the side.");
 console.log(War + " rushed to his feet, found his sword in a frantic, and lunged to pick up the ebon blade, and then readied himself for " + Htr + ".");
 console.log(War + " face gave a puzzling look as he realized the " + Class[0] + " is nowhere in sight.  The " + Class[2] + " stood motionless, surveying the area.");
+console.log("From behind, " + Htr + " arose from a thicket of grass with great agility, " + Arrows + " arrows in an open quiver and his bow at the ready for Rapid Fire.");
+console.log(Htr + " pulls four arrows for each area he's focused on.");
 //Array Function (Array Arguments, Object argument, mutator	, array prop, function(procedure hopefully), local var, for loop, nested loop, math, output, accessor, array return)			
-var Arrow = 4,
-Aim = ["shoulder", "forearm", "knee"],
+var 
+Aim = ["shoulders", "forearms", "knees"],
 Notch = [1,2,3,4];
-Aim.push("ground");
-
-
-
-/*needed
-Number Argument
-Number Prop
-Number return Value
-While loop
-Returned Values
-procuedure (if nto used yet)
-Finish*/
+Aim.push("feet"),
+Wound = {
+	miss: "feet",
+	low: "knee",
+	middle: "forearm",
+	high: "shoulder"
+};
+for (var i = 0, j=Aim.length; i<j; i++)
+	for (var k = 0, l = Notch.length; k<l; k++){
+		console.log(Htr + " sets to notch arrow number " + Notch[k])
+	console.log("and let fly, focused on " + War + "'s " + Aim[i]);
+};
+var Ouch = Wound.low;
+console.log (War + "'s shield could not get all the arrows, but did block most. \"An arrow to the " + Ouch + "? Not very original...\"");
+console.log (War + " pull out the two arrows that made it to his shoulder and the one above his knee.  The one in his forearm is deep, so he cuts the shaft off from his body for now.");
+console.log("\"Allow me to return the favor that you've gracioulsy have given me!\" " + War + " yelled as he started rushing towards " + Htr + ".");
+console.log(War + " unleashes a flurry of sword swings on " + Htr + ".");
+//Number (Number, prop, while loop, math
+while (maxSlash < 6){
+		console.log(War + " attacks with swing " + maxSlash);
+		maxSlash++;
+};
+console.log(Htr + " tried again to block each hit, but finally gave out from the weight of the sword on his bow that the fifth attack managed to make it's way through,");
+console.log("the tip of the blade making it's mark along " + Htr + "'s chest, the bow snapping, the " + Class[0] + " dropping to his knees.");
+console.log("Both the orcs were motionless save for the heavy panting of catching their breaths from such swift and fruious attacks.");
+console.log(War + " extended his hand out to " + Htr + " caked in dried black blood from the arrow hit. \"It's not a serious wound, don't be upset.\"");
+var endSpar = function(SparComplete) {
+	var tiredOrcs = 2,
+		deadOrc,
+		DrawEnd;
+		if (tiredOrcs = 2){ 
+		deadOrc = 0; 
+		} else {deadOrc = 1};
+	DrawEnd = Htr + " clasped " + War + "'s hand, the helping hand pulling him up to his feet.";
+	return DrawEnd;
+	};
+var MatchSet = endSpar();
+console.log(MatchSet);
+console.log("\"It's not the cut that wounds me but the bow...you know you'll be owing me a new one.\" " + Htr + " cackled.");
+console.log("\"Lets say we fight over who pays on the morrow.\" " + War + " grinned.");
